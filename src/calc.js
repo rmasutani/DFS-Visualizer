1f90415b08b8f14c1de720abbfebb669fd04af5a
rmasutani <masutanirugby@gmail.com>
2022-06-06 16:48:09 +0900

    Add eventlistener to change cell color

--- src/calc.js
+++ src/calc.js
@@ -18,10 +18,27 @@ function create_board() {
   const row = parseInt(document.getElementById("row").value);
   const col = parseInt(document.getElementById("col").value);
 
+  //   let id = 0;
   for (let i = 0; i < row; i++) {
     const tr = document.createElement("tr");
     for (let j = 0; j < col; j++) {
       const td = document.createElement("td");
+      td.setAttribute("id", `cell_${i}_${j}`);
+
+      // Add onclick eventListener
+      td.addEventListener("click", function () {
+        let cell_color = document.getElementById(`cell_${i}_${j}`).style
+          .backgroundColor;
+
+        if (!cell_color) {
+          document.getElementById(`cell_${i}_${j}`).style.backgroundColor =
+            "black";
+        } else {
+          document.getElementById(`cell_${i}_${j}`).style.backgroundColor =
+            "white";
+        }
+      });
+
       tr.appendChild(td);
     }
     table.appendChild(tr);
